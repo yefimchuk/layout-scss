b5964db2919b8b54a82b256639ae6f9472ad1ffd
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-11-19 23:46:49 +0300

    fihish functionality

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ document.querySelector("#bb").addEventListener("click", function () {
     document.querySelector("#bb").setAttribute("aria-expanded","true")
     console.log("he")
 })
+document.querySelector("#bb").addEventListener("click", function () {
+
+    document.querySelector("#bb").setAttribute("aria-expanded","true")
+    console.log("he")
+})document.querySelector("#bb").addEventListener("click", function () {
+
+    document.querySelector("#bb").setAttribute("aria-expanded","true")
+    console.log("he")
+})
 */
+let a = document.querySelector("body")
